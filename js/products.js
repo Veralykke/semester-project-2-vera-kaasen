e9141abb29c5e5b1628dc796460c1f89cc800bd5
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2020-11-23 22:28:56 +0100

    details

--- js/products.js
+++ js/products.js
@@ -1,12 +1,11 @@
 import {hamburgerMenu} from "./hamburger.js";
 import {url} from "./components/api.js";
 import { searchProducts } from "./components/common/searchProducts.js";
-/*import { renderProducts } from "./components/common/renderProducts.js";
-
+/*
 import { displayMessage } from "./components/common/displayMessage.js";*/
 
 
-/*NY API VERSjON PLUSS ADDING TO CART*/
+/* NY API VERSjON */
 /*Adding HTML(json) function here*/
 
 const search = document.querySelector(".search-box");
@@ -56,50 +55,6 @@ function viewDetails(e){
     window.location.href = `detail.html?id=${e.target.dataset.id}`;
 }
 
-/*
-const productContainer = document.querySelector(".product-container");
-
-function renderProducts()  {
-
-    productContainer.innerHTML = "";
-
-    renderProductsUrl.forEach(function (product) {
-        ProductContainer.innerHTML += 
-
-        container.innerHTML += `
-        <div id="product-${product.id}" class="product" 
-            data-title = "${product.title}" 
-            data-price = "${product.price}"
-            data-image = "http://localhost:1337${product.image.url}">
-
-            <h4>${product.title}</h4>
-            <p>Price: ${product.price}</p>
-            <img class="product-images" src="http://localhost:1337${product.image.url}" alt="product-images">
-            <button onclick href= details.html class= "add-to-cart" = "addCart(${product.id})">More</button>
-        </div>`;
-        
-    });
-}
-
-
-renderProducts();
-
-
-search.onkeyup = function () {
-    //console.log(event);
-
-    const searchProduct = event.target.value.trim().toLowerCase();
-
-    const filteredProducts = renderProductsUrl.filter(function(product) {
-        if(product.full_name.toLowerCase().startsWith(searchProduct)) {
-            return true;
-        }
-    })
-
-    console.log(filteredProducts);
-
-    renderProducts();
-};*/
 
 
 
@@ -124,44 +79,6 @@ search.onkeyup = function () {
 
 
 
-
-
-
-
-
-/*ANNA VERSJON---SE PÅ*/
-
-/*
-const productsUrl = url  + "products";
-
-async function getProducts() {
-    
-    try {
-        const response = await fetch(productsUrl);
-        const json = await response.json();
-      
-        console.log(json.data);
-
-        const products = json.data;
-
-        renderProducts(products);
-        searchProducts(products);
-    }catch (error) {
-        console.log(error);
-        displayMessage("error", error, ".product-container");
-    }
-}
-
-getProducts();
-
-/*BUTTON TIL DETAilpage---FIX
-const poroductByID = product.find.(findProduct);
-
-function findProduct(product) {
-    if(product.id === 1) {
-        return true;
-    }
-}*/
 
 
 
